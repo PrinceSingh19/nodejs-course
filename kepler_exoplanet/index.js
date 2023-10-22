b13ade8df61ebfe2be85990c9dac18fb9ee443c5
Prince Singh <prince@performena.com>
2023-10-22 21:59:29 +0530

    added the kepler exoplanet data

---
+++ kepler_exoplanet/index.js
@@ -0,0 +1,32 @@
+const { parse } = require("csv-parse");
+const fs = require("fs");
+
+const isHabitablePlanet = (planet) => {
+	return (
+		planet["koi_disposition"] === "CONFIRMED" &&
+		planet["koi_insol"] > 0.36 &&
+		planet["koi_insol"] < 1.11 &&
+		planet["koi_prad"] < 1.6
+	);
+};
+
+const habitablePlanet = [];
+fs.createReadStream("kepler_data.csv")
+	.pipe(
+		parse({
+			comment: "#",
+			columns: true,
+		})
+	)
+	.on("data", (data) => {
+		if (isHabitablePlanet(data)) {
+			habitablePlanet.push(data);
+		}
+	})
+	.on("error", (error) => console.log(error))
+	.on("end", () => {
+		console.log(
+			"habitable planet",
+			habitablePlanet.map((planet) => planet["kepler_name"])
+		);
+	});
