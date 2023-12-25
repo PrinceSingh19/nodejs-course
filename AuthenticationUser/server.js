f4f58d2f61a4b2172a71ac05efa0553f109214f1
Prince Singh <princesingh808087@gmail.com>
2023-12-25 11:36:17 +0530

    completed the logout and temple engine fucntionality in nodejs

--- AuthenticationUser/server.js
+++ AuthenticationUser/server.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const https = require("https");
+const path = require("path");
 const express = require("express");
 const helmet = require("helmet");
 const passport = require("passport");
@@ -8,8 +9,14 @@ const cookieParser = require("cookie-parser");
 const { authenticate, passportAuth } = require("./passport");
 const loginRouter = require("./routes/loginRouter");
 
+// create app instance from express
 const app = express();
+
+// specify the port number for your app
 const PORT = 3000;
+
+// using this helmet middleware to set extra attributes on the request headers
+app.use(helmet());
 
 // middleware to parse the cookies in the incoming requests
 app.use(cookieParser());
@@ -17,21 +24,23 @@ app.use(cookieParser());
 // including the passportjs middleware
 passportAuth();
 
-// using this helmet middleware to set extra attributes on the request headers
-app.use(helmet());
 app.use(express.urlencoded({ extended: false }));
 
 // initialising the passportjs middleware
 app.use(passport.initialize());
 
 // home page route
-app.get("/", (req, res) => res.send("Hi"));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, ".", "public", "home.html")));
 
 // secret page route which requires the user to be authenticated
 app.get("/secret", authenticate, (req, res) => {
 	res.send("secret value is 19");
 });
 
+app.get("/logout", (req, res, next) => {
+	res.clearCookie("secure_token"); // Clear the access token cookie
+	res.redirect("/login");
+});
 // login route
 app.use("/login", loginRouter);
 // https
--- MVC/index.js
+++ MVC/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const friendsRouter = require("./routers/friends.router");
 const messagesRouter = require("./routers/messages.router");
+const path = require("path");
 
 const app = express();
-app.use(express.json());
 const PORT = 3000;
+
+app.use(express.json());
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use((req, res, next) => {
 	const start = Date.now();
@@ -13,10 +18,28 @@ app.use((req, res, next) => {
 	console.log(`${req.method} ${req.url} ${Delta}`);
 });
 
+// way to send the html files without using render method and template engines
+// app.use(express.static(path.join(__dirname, "public")));
+// app.get("/", (req, res) => {
+// 	res.sendFile(path.join(__dirname, ".", "public", "index.html"));
+// });
+
+// another way to send the static website with some dynamic data
+app.use("/site", express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-	res.send("<p>Hi my name is <strong>Prince Singh</strong></p>");
+	res.render("index", {
+		title: "Nodejs course",
+		description: "You did it Prince!",
+	});
 });
 
+app.get("/messages", (req, res) => {
+	res.render("messages", {
+		title: "Nodejs course",
+		message: "Keep doing this Prince. One day you'll make it",
+	});
+});
 app.use("/friends", friendsRouter);
 app.use("/messages", messagesRouter);
 
